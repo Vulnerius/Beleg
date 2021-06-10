bb7763caa8e33e9f5344f73e896e706ddf561d1e
PaulFuchs <pfuchs480@gmail.com>
2021-06-10 13:08:23 +0200

    :white_check_mark: added shoTesting to PlayerTest

--- src/test/java/TestPlayer.java
+++ src/test/java/TestPlayer.java
@@ -2,6 +2,8 @@ import de.hsmw.kriegZurSee.GameComponents.Player;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.awt.*;
+
 public class TestPlayer {
     Player playerOne = new Player("Player 1");
     Player playerTwo = new Player("Player 2");
@@ -11,8 +13,15 @@ public class TestPlayer {
         Assertions.assertEquals("Player 1", playerOne.name);
     }
 
-    @Test public void setEnemyForPlayerTwo(){
+    @Test
+    public void setEnemyForPlayerTwo(){
         playerTwo.setEnemy(playerOne);
         Assertions.assertEquals(playerOne, playerTwo.enemy);
+    }
+    @Test
+    public void shotTesting(){
+        playerOne.setEnemy(playerTwo);
+        Assertions.assertTrue(playerOne.shootOne(new Point(2,0)));
+        Assertions.assertFalse(playerOne.shootOne(new Point(1,0)));
     }
 }
