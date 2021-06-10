e0af9bc1b02d2962a485c2fb6346c3962b351e86
PaulFuchs <pfuchs480@gmail.com>
2021-06-10 03:16:35 +0200

    :white_check_mark: added Test for Field

--- src/test/java/TestField.java
+++ src/test/java/TestField.java
@@ -1,7 +1,10 @@
+import de.hsmw.kriegZurSee.Boats.AbstractBoat;
 import de.hsmw.kriegZurSee.GameComponents.Field;
 import org.apache.commons.lang3.Range;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.awt.*;
 
 public class TestField {
 
@@ -13,9 +16,19 @@ public class TestField {
         Assertions.assertTrue(Range.between(1,6).contains((int) fieldOne.searchFct().getX()));
         Assertions.assertTrue(Range.between(1,6).contains((int) fieldOne.searchFct().getY()));
 
+        fieldOne.bs.addHitPoint(fieldOne.bs.getPosition());
+        fieldOne.cs.addHitPoint(fieldOne.cs.getPosition());
         fieldOne.rb2.addHitPoint(fieldOne.rb2.getPosition());
-        
+        fieldOne.rb1.addHitPoint(fieldOne.rb1.getPosition());
+        fieldOne.hlb.addHitPoint(fieldOne.hlb.getPosition());
+
+        Point gottenPoint = fieldOne.searchFct();
+        for(AbstractBoat b : fieldOne.boats){
+            Assertions.assertNotEquals(gottenPoint, b.getPosition());
+        }
+
     }
+
     @Test
     public void allBoatsInField(){
         Assertions.assertTrue(fieldTwo.allBoatsInField());
