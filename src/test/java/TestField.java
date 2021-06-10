f04672ceb0795ecffc17430690390607622cacdf
PaulFuchs <pfuchs480@gmail.com>
2021-06-10 02:45:01 +0200

    :tada: added TestFile

---
+++ src/test/java/TestField.java
@@ -0,0 +1,20 @@
+import de.hsmw.kriegZurSee.GameComponents.Field;
+import org.apache.commons.lang3.Range;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestField {
+
+    Field fieldOne = new Field();
+    Field fieldTwo = new Field();
+
+    @Test
+    public void searchForBoat(){
+        Assertions.assertTrue(Range.between(1,6).contains((int) fieldOne.searchFct().getX()));
+        Assertions.assertTrue(Range.between(1,6).contains((int) fieldOne.searchFct().getY()));
+    }
+    @Test
+    public void allBoatsInField(){
+        Assertions.assertTrue(fieldTwo.allBoatsInField());
+    }
+}
