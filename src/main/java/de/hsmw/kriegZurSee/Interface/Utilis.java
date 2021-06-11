cddc476542912a3059bca4857229d99391b5e5dc
PaulFuchs <pfuchs480@gmail.com>
2021-06-11 12:29:07 +0200

    :sparkles: added gridMaking to both Field added Textappend on consoleView

--- src/main/java/de/hsmw/kriegZurSee/Interface/Utilis.java
+++ src/main/java/de/hsmw/kriegZurSee/Interface/Utilis.java
@@ -21,21 +21,5 @@ public interface Utilis {
 
 
     //copy Paste from stackoverflow
-     static Pane makeGridToPane(Pane p, int n){
-        double width = p.getWidth()/n;
-        javafx.scene.shape.Rectangle[][] rectangles = new javafx.scene.shape.Rectangle[n][n];
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                rectangles[i][j] = new Rectangle();
-                rectangles[i][j].setX(i * width);
-                rectangles[i][j].setY(j * width);
-                rectangles[i][j].setWidth(width);
-                rectangles[i][j].setHeight(width);
-                rectangles[i][j].setFill(javafx.scene.paint.Color.BLUE);
-                rectangles[i][j].setStroke(Color.BLACK);
-                p.getChildren().add(rectangles[i][j]);
-            }
-        }
-        return p;
-    }
+
 }
--- src/main/java/de/hsmw/kriegZurSee/MainScene/Controller.java
+++ src/main/java/de/hsmw/kriegZurSee/MainScene/Controller.java
@@ -1,8 +1,13 @@
 package de.hsmw.kriegZurSee.MainScene;
 
+import de.hsmw.kriegZurSee.GameComponents.Player;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
 
 
 
@@ -19,6 +24,43 @@ public class Controller {
     public ChoiceBox actionChoice;
     public Button go;
     public TextArea consoleView;
+    public Scene MainScene;
+    public Stage mainStage;
+
+    public void onShowing(){
+        mainStage.setResizable(false);
+        makeGridToPane(myGrid, 6);
+        makeGridToPane(enemyGrid, 6);
+    }
+    public void onButtonClicked(){
+        consoleView.appendText(shootX.getText() + " " + shootY.getText() + "\n");
+    }
 
 
+   public static Pane makeGridToPane(Pane p, int n){
+        double width = p.getPrefHeight()/n;
+        javafx.scene.shape.Rectangle[][] rectangles = new javafx.scene.shape.Rectangle[n][n];
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                rectangles[i][j] = new Rectangle();
+                rectangles[i][j].setX(i * width);
+                rectangles[i][j].setY(j * width);
+                rectangles[i][j].setWidth(width);
+                rectangles[i][j].setHeight(width);
+                rectangles[i][j].setFill(javafx.scene.paint.Color.BLUE);
+                rectangles[i][j].setStroke(Color.BLACK);
+                p.getChildren().add(rectangles[i][j]);
+            }
+        }
+       p.setOnMouseClicked(me -> {
+           double posX = me.getX();
+           double posY = me.getY();
+
+           int colX = (int) (posX / width);
+           int colY = (int) (posY / width);
+
+           rectangles[colX][colY].setFill(Color.RED);
+       });
+        return p;
+    }
 }
