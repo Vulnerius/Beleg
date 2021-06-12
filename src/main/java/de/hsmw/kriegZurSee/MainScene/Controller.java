a28c67bc9ca3e6998cf4075470404976ec55110a
PaulFuchs <pfuchs480@gmail.com>
2021-06-12 03:19:29 +0200

    :fire: added Stylesheet to mainScene - tried to access Gridpane but failed

--- src/main/java/de/hsmw/kriegZurSee/MainScene/Controller.java
+++ src/main/java/de/hsmw/kriegZurSee/MainScene/Controller.java
@@ -1,20 +1,22 @@
 package de.hsmw.kriegZurSee.MainScene;
 
 import de.hsmw.kriegZurSee.GameComponents.Player;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
-
+import java.awt.event.MouseEvent;
 
 
 public class Controller {
     public Pane myGrid;
-    public Pane enemyGrid;
+    public GridPane enemyGrid;
     public Label shotCount;
     public Label searchPoint;
     public ImageView boat1Image;
@@ -27,21 +29,21 @@ public class Controller {
     public Scene MainScene;
     public Stage mainStage;
 
-    public void onShowing(){
+    public void onShowing() {
         mainStage.setResizable(false);
         makeGridToPane(myGrid, 6);
-        makeGridToPane(enemyGrid, 6);
-    }
-    public void onButtonClicked(){
-        consoleView.appendText(shootX.getText() + " " + shootY.getText() + "\n");
     }
 
 
-   public static Pane makeGridToPane(Pane p, int n){
-        double width = p.getPrefHeight()/n;
-        javafx.scene.shape.Rectangle[][] rectangles = new javafx.scene.shape.Rectangle[n][n];
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
+    public void accessGridPane() {
+
+    }
+
+    public static Pane makeGridToPane(Pane p, int n) {
+        double width = p.getPrefHeight() / n;
+        Rectangle[][] rectangles = new Rectangle[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 rectangles[i][j] = new Rectangle();
                 rectangles[i][j].setX(i * width);
                 rectangles[i][j].setY(j * width);
@@ -52,15 +54,19 @@ public class Controller {
                 p.getChildren().add(rectangles[i][j]);
             }
         }
-       p.setOnMouseClicked(me -> {
-           double posX = me.getX();
-           double posY = me.getY();
+        p.setOnMouseClicked(me -> {
 
-           int colX = (int) (posX / width);
-           int colY = (int) (posY / width);
+                    double posX = me.getX();
+                    double posY = me.getY();
 
-           rectangles[colX][colY].setFill(Color.RED);
-       });
+                    int colX = (int) (posX / width);
+                    int colY = (int) (posY / width);
+
+                    rectangles[colX][colY].setFill(Color.RED);
+                }
+
+        );
         return p;
     }
+
 }
